0b92f67357c0386913319a70d8d0fc4e02fefe72
Uji <uji@github.com>
2022-03-01 19:35:49 +0200

    modified url patterns

--- contacts/urls.py
+++ contacts/urls.py
@@ -3,6 +3,6 @@ from django.views.generic import TemplateView
 
 app_name = 'contacts'
 
-url_patterns = [
+urlpatterns = [
     path('', TemplateView.as_view(template_name='contacts/index.html'))
 ]
--- contacts_api/urls.py
+++ contacts_api/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from views import ContactListView, ContactCreateView, ContactUpdateView, ContactDeleteView
+from .views import ContactListView, ContactCreateView, ContactUpdateView, ContactDeleteView
 
 app_name = 'contacts_api'
 
-url_patterns = [
+urlpatterns = [
     path('', ContactListView.as_view(), name='all'),
     path('contacts/create/', ContactCreateView.as_view(), name='contact_create'),
     path('contacts/<int:pk>/update/', ContactUpdateView.as_view(), name='contact_update'),
