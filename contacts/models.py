6e348e728986abafd16743cfa1ebc302a91bab11
Uji <uji@github.com>
2022-03-01 19:24:50 +0200

    added views and serializers

--- contacts/models.py
+++ contacts/models.py
@@ -6,6 +6,7 @@ class Contact(models.Model):
     name = models.CharField(max_length=50)
     email = models.EmailField(max_length=254)
     phone = models.CharField(max_length=15) # CharField used for simplicity, better implementation could use RegexField
-
+    objects = models.Manager()
+    
     def __str__(self):
         return self.name
--- contacts_api/views.py
+++ contacts_api/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework import generics
+from contacts.models import Contact
+from .serializers import ContactSerializer
 
 # Create your views here.
+class ContactListView(generics.ListAPIView):
+    queryset = Contact.objects.all()
+    serializer_class = ContactSerializer
+    pass
+
+class ContactCreateView(generics.CreateAPIView):
+    pass
+
+class ContactUpdateView(generics.UpdateAPIView):
+    pass
+
+class ContactDeleteView(generics.DestroyAPIView):
+    pass
