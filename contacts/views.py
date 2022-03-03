8caf0d571eb286387e9206c07ec4cd2e200eeaad
Uji <uji@github.com>
2022-03-03 02:26:00 +0200

    fix: changed generic views to viewsets

--- contacts/views.py
+++ contacts/views.py
@@ -1,20 +1,8 @@
-from rest_framework import generics
+from rest_framework import viewsets
 from contacts.models import Contact
 from contacts.serializers import ContactSerializer
 
 # Create your views here.
-class ContactListView(generics.ListAPIView):
-    queryset = Contact.objects.all()
-    serializer_class = ContactSerializer
-
-class ContactCreateView(generics.ListCreateAPIView):
-    queryset = Contact.objects.all()
-    serializer_class = ContactSerializer
-
-class ContactUpdateView(generics.RetrieveUpdateAPIView):
-    queryset = Contact.objects.all()
-    serializer_class = ContactSerializer
-
-class ContactDeleteView(generics.RetrieveDestroyAPIView):
+class ContactViewSet(viewsets.ModelViewSet):
     queryset = Contact.objects.all()
     serializer_class = ContactSerializer
