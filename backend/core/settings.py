a91c0bdf471be308341dc77fbf975f060450f40d
Uji <uji@github.com>
2022-03-04 00:16:18 +0200

    feat: installed django cors headers

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -40,7 +40,10 @@ INSTALLED_APPS = [
 
     'contacts',
     'rest_framework',
+    'corsheaders',
 ]
+
+CORS_ORIGIN_ALLOW_ALL = True
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -50,6 +53,9 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
 ]
 
 ROOT_URLCONF = 'core.urls'
